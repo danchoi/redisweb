fe9c384d73beab972f6fde3e286f86a1042226f3
Daniel Choi <dhchoi@gmail.com>
2016-08-02 13:11:01 -0400

    add deps

--- Main.hs
+++ Main.hs
@@ -1,4 +1,9 @@
 module Main where
+import Options.Applicative
+import qualified Database.Redis.Redis as R
+import Data.Text (Text)
+import qualified Data.ByteString.Char8 as B8
+import qualified Data.ByteString.Lazy.Char8 as BL8
 
 main :: IO ()
 main = do
